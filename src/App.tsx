4b5c1405f362d23bfa856b6e808b9cd1511f8337
Fernan007do <Fernan007do@users.noreply.github.com>
2023-04-21 13:53:03 -0300

    jugando un poco con M UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, IconButton, Stack } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+
 
 function App() {
   return (
@@ -9,7 +15,28 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+
         </p>
+          <Button size= "small" variant="text">Text</Button>
+          <Button size= "medium" variant="contained">Contained</Button>
+          <Button size= "large" variant="outlined">Outlined</Button>
+          <IconButton aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+          <Stack direction="row" spacing={1}>
+            <IconButton aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
+            <IconButton aria-label="delete" disabled color="primary">
+              <DeleteIcon />
+            </IconButton>
+            <IconButton color="secondary" aria-label="add an alarm">
+              <AlarmIcon />
+            </IconButton>
+            <IconButton color="primary" aria-label="add to shopping cart">
+              <AddShoppingCartIcon />
+            </IconButton>
+          </Stack>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +46,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
