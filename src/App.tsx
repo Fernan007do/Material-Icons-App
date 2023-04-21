f93627a90b438cd06dfd1148d8fe06b256fe2a84
Fernan007do <Fernan007do@users.noreply.github.com>
2023-04-21 14:05:27 -0300

    borrando archivos innecesarios

--- src/App.tsx
+++ src/App.tsx
@@ -11,41 +11,6 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-
-        </p>
-          <Button size= "small" variant="text">Text</Button>
-          <Button size= "medium" variant="contained">Contained</Button>
-          <Button size= "large" variant="outlined">Outlined</Button>
-          <IconButton aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
-          <Stack direction="row" spacing={1}>
-            <IconButton aria-label="delete">
-              <DeleteIcon />
-            </IconButton>
-            <IconButton aria-label="delete" disabled color="primary">
-              <DeleteIcon />
-            </IconButton>
-            <IconButton color="secondary" aria-label="add an alarm">
-              <AlarmIcon />
-            </IconButton>
-            <IconButton color="primary" aria-label="add to shopping cart">
-              <AddShoppingCartIcon />
-            </IconButton>
-          </Stack>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       
     </div>
   );
